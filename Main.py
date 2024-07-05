f4dfd3222686e53b6e01cbd0d2276dd0dc6c94cb
42ccode <42ccode@gmail.com>
2024-07-05 19:39:11 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ import os
 files = os.listdir('img_1')  # 传如文件夹路径
 mark = input("请输入您要添加的水印信息:")
 for file in files:
-    add_mark(f'img_1/{file}', mark=mark, opacity=0.5)  # mark为水印信息
+    add_mark(f'img_1/{file}', mark=mark, opacity=0.5) 
